6096a3040509b4030a927198067dc338bc023db2
Harmelodic <matt@harmelodic.com>
2022-07-04 22:26:15 +0200

    Initial provider

---
+++ src/provider.tf
@@ -0,0 +1,58 @@
+terraform {
+  required_version = ">=1.1.8"
+
+  backend "gcs" {
+    bucket = "harmelodic-tfstate"
+    prefix = "personal-env-infrastructure"
+  }
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.27.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "4.27.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.12.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.6.0"
+    }
+  }
+}
+
+variable "region" {
+  default     = "europe-north1"
+  description = "GCP Region"
+  sensitive   = true
+  type        = string
+}
+
+provider "google" {
+  region = var.region
+}
+
+provider "google-beta" {
+  region = var.region
+}
+
+data "google_client_config" "current" {}
+#
+#provider "kubernetes" {
+#  host                   = "https://${google_container_cluster.apps.endpoint}"
+#  cluster_ca_certificate = base64decode(google_container_cluster.apps.master_auth.0.cluster_ca_certificate)
+#  token                  = data.google_client_config.current.access_token
+#}
+#
+#provider "helm" {
+#  kubernetes {
+#    host                   = "https://${google_container_cluster.apps.endpoint}"
+#    cluster_ca_certificate = base64decode(google_container_cluster.apps.master_auth.0.cluster_ca_certificate)
+#    token                  = data.google_client_config.current.access_token
+#  }
+#}
