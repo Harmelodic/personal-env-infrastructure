7c095015d958def570c4b6e11dda3026a0ec227c
Matt Smith <matt@harmelodic.com>
2022-07-05 11:16:55 +0200

    Update apps-access-artifacts.tf

--- src/apps-access-artifacts.tf
+++ src/apps-access-artifacts.tf
@@ -7,11 +7,21 @@ data "google_project" "personal_artifacts" {
 }
 
 # Harmelodic Artifacts
-resource "google_artifact_registry_repository_iam_member" "gke_node_service_agent" {
+resource "google_artifact_registry_repository_iam_member" "apps_compute_harmelodic" {
   provider   = google-beta
   project    = data.google_project.personal_artifacts.project_id
   location   = var.region
   repository = "harmelodic"
+  role       = "roles/artifactregistry.reader"
+  member     = "serviceAccount:${google_project.apps.number}-compute@developer.gserviceaccount.com"
+}
+
+# Mary Website Artifacts
+resource "google_artifact_registry_repository_iam_member" "apps_compute_mary_website" {
+  provider   = google-beta
+  project    = data.google_project.personal_artifacts.project_id
+  location   = "asia-southeast1"
+  repository = "mary-website"
   role       = "roles/artifactregistry.reader"
   member     = "serviceAccount:${google_project.apps.number}-compute@developer.gserviceaccount.com"
 }
