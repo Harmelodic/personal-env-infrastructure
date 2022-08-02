eb88c56731509056f678100b04041bd10de0bc1d
Harmelodic <matt@harmelodic.com>
2022-08-02 19:23:11 +0200

    resource clarification

--- src/host-network.tf
+++ src/host-network.tf
@@ -15,11 +15,11 @@ resource "google_compute_route" "main_default_internet_gateway" {
   project          = google_project.host.project_id
 }
 
-resource "google_compute_global_address" "private_service_access" {
+resource "google_compute_global_address" "private_google_services_access" {
   address_type  = "INTERNAL"
   description   = "A global address for Private Google Services Access"
   ip_version    = "IPV4"
-  name          = "private-service-access"
+  name          = "private-google-services-access"
   network       = google_compute_network.main.id
   prefix_length = 20
   project       = google_project.host.project_id
@@ -28,6 +28,6 @@ resource "google_compute_global_address" "private_service_access" {
 
 resource "google_service_networking_connection" "private_vpc_connection" {
   network                 = google_compute_network.main.id
-  reserved_peering_ranges = [google_compute_global_address.private_service_access.name]
+  reserved_peering_ranges = [google_compute_global_address.private_google_services_access.name]
   service                 = "servicenetworking.googleapis.com"
 }
