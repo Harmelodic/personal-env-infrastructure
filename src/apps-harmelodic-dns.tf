16ae44ab4d7c8fa97e0e95050d2de5e9b4fab289
Harmelodic <matt@harmelodic.com>
2023-07-02 19:44:59 +0200

    Comment for OSSRH and removing personal custom TXT record

--- src/apps-harmelodic-dns.tf
+++ src/apps-harmelodic-dns.tf
@@ -73,7 +73,6 @@ resource "google_dns_record_set" "harmelodic_com_txt" {
   rrdatas = [
     "google-site-verification=jXVba9WLVzprbkW4EpS3vtWL5-2YH03AwSc8sprMfSU",
     "keybase-site-verification=sDm605nNkmQuRsuciUxr9KmDkMgBVUKD5Ea38C_8L4w",
-    "extra-custom-value=hello-matt",
     "OSSRH-92980" # https://issues.sonatype.org/browse/OSSRH-92980
   ]
 }
