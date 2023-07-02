2e7b8bc6443b0dfebe7c1e4463cad33490d31830
Harmelodic <matt@harmelodic.com>
2023-07-02 19:43:42 +0200

    OSSRH TXT record

--- src/apps-harmelodic-dns.tf
+++ src/apps-harmelodic-dns.tf
@@ -73,6 +73,7 @@ resource "google_dns_record_set" "harmelodic_com_txt" {
   rrdatas = [
     "google-site-verification=jXVba9WLVzprbkW4EpS3vtWL5-2YH03AwSc8sprMfSU",
     "keybase-site-verification=sDm605nNkmQuRsuciUxr9KmDkMgBVUKD5Ea38C_8L4w",
-    "extra-custom-value=hello-matt"
+    "extra-custom-value=hello-matt",
+    "OSSRH-92980"
   ]
 }
